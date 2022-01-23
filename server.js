f346c8fd0776236a99999d349101fd8e7c275ce2
unknown <kvaruas@gmail.com>
2022-01-23 17:57:38 +0530

    second commit

--- server.js
+++ server.js
@@ -1,11 +1,37 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan'); 
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 //load env vars 
-dotenv.config({path : './config/config.env'});
+dotenv.config({path : './config/config.env'}); 
+//connect to DB 
+connectDB();
+// Route files 
 
-const app = express();
+const bootcamps = require('./routes/bootcamps');
 
+const app = express(); 
+//Body parser 
+app.use(express.json());
+
+
+//Dev logging middlware 
+if(process.env.NODE_ENV === 'development')
+{
+    app.use(morgan('dev'));
+}
+
+//Mount routers 
+app.use('/api/v1/bootcamps',bootcamps);
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} mode on PORT ${PORT}`));
+const server =app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} mode on PORT ${PORT}`.yellow.bold));
+
+//Handle unhandled promise rejection 
+process.on('unhandledRejection',(err,promise)=>{
+    console.log(`Error : ${err.message}`.red); 
+    //close server and exit process 
+    server.close(()=>process.exit(1));
+})
